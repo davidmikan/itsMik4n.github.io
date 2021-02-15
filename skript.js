1c3b0d0702a1a1247c777b9d5b5ef1b8a0460eaa
David Mikan <itsmikanakg@gmail.com>
2021-02-15 16:25:02 +0100

    giveaway for tobias
    yes free ifon12! yes

---
+++ skript.js
@@ -0,0 +1,9 @@
+var it = 0;
+var color = ["red", "blue", "lime", "cyan", "magenta", "yellow"];
+
+function update() {
+    it++;
+    document.getElementById("bodi").style = "background-color: " + color[it % color.length] + ";";
+}
+
+setInterval(update, 500);
