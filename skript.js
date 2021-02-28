52c76b195e799100b353badda4dab25a453c219f
David Mikan <itsmikanakg@gmail.com>
2021-02-28 20:01:17 +0100

    i

--- skript.js
+++ skript.js
@@ -1,9 +1,9 @@
-var it = 0;
-var color = ["red", "blue", "lime", "cyan", "magenta", "yellow"];
+// var it = 0;
+// var color = ["red", "blue", "lime", "cyan", "magenta", "yellow"];
 
-function update() {
-    it++;
-    document.getElementById("bodi").style = "background-color: " + color[it % color.length] + ";";
-}
+// function update() {
+//     it++;
+//     document.getElementById("bodi").style = "background-color: " + color[it % color.length] + ";";
+// }
 
-setInterval(update, 500);
+// setInterval(update, 500);
