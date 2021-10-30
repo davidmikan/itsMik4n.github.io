354c18fc570eab6f5d79ff1ce8a7ee26ed833730
David Mikan <itsmikanakg@gmail.com>
2021-10-30 19:09:59 +0200

    deleted nonsense, put up lebenslauf

--- skript.js
+++
@@ -1,9 +0,0 @@
-// var it = 0;
-// var color = ["red", "blue", "lime", "cyan", "magenta", "yellow"];
-
-// function update() {
-//     it++;
-//     document.getElementById("bodi").style = "background-color: " + color[it % color.length] + ";";
-// }
-
-// setInterval(update, 500);
